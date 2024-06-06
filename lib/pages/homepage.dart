22d3438d1b59776f0b116276fae5df138638a391
gilbert-ku <gilbert45ku@gmail.com>
2024-06-07 00:32:56 +0300

    add log in image

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -15,9 +15,9 @@ class HomePage extends StatelessWidget {
           child: Center(
             child: ListView(
               // heased
-              children: const [
+              children: <Widget> [
                 // title
-                Center(
+                const Center(
                   child: Text("EstateEase App",
                   style: TextStyle(
                     color: Colors.white,
@@ -26,7 +26,30 @@ class HomePage extends StatelessWidget {
                     fontWeight: FontWeight.bold
                   ),
                   ),
-                )
+                ),
+
+
+                const SizedBox(height: 5,),
+
+                // welcome text
+                const Center(
+                  child: Text("Welcome Back!!",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300
+                  ),
+                  ),
+                ),
+
+                // image
+                Center(
+                  child:  Image.asset("lib/images/login.png",
+                  width: 150.0,
+                  height: 300.0,
+                  ),
+                ),
+                // input field
               ],
             ),
           ),
