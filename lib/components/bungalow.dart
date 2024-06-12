7d1b03faedad95e191ce8b58e74b62ff850ce36e
gilbert-ku <gilbert45ku@gmail.com>
2024-06-12 22:06:06 +0300

    add components

---
+++ lib/components/bungalow.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Bungalow extends StatelessWidget {
+  const Bungalow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/components/mansion.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Mansion extends StatelessWidget {
+  const Mansion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
