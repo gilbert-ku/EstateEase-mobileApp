1320b9e0d54cbc0e117ead2ba4a9a8556d2581c5
gilbert-ku <gilbert45ku@gmail.com>
2024-06-14 12:54:42 +0300

    add drower

--- lib/pages/house_listing.dart
+++ lib/pages/house_listing.dart
@@ -8,57 +8,59 @@ class HouseListing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: const Color.fromARGB(255, 3, 99, 179),
       appBar: AppBar(
-        
-        leading:  IconButton(
-          icon: const Icon(Icons.menu,
-          color: Colors.white,
-          size: 40,
-          ), 
-          onPressed: () {},
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+              size: 40,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+            },
+          ),
         ),
+                
         title: const Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
-            child: TextField(
-              decoration: InputDecoration(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
+          child: TextField(
+            decoration: InputDecoration(
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  borderSide: BorderSide.none
-                ),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    borderSide: BorderSide.none),
                 hintText: "Search property based on location and bedroom",
                 fillColor: Colors.white,
-                filled: true
-              ),
-              
-            ),
-          )
-        ),
+                filled: true),
+          ),
+        )),
         elevation: 0,
         backgroundColor: const Color.fromARGB(255, 3, 99, 179),
       ),
 
+      drawer: const Drawer(),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
           child: ListView(
-            children: <Widget> [
-              const Text("Welcome Gilbert, We have made your house hunting Easy.",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white
+            children: <Widget>[
+              const Text(
+                "Welcome Gilbert, We have made your house hunting Easy.",
+                style: TextStyle(fontSize: 16, color: Colors.white),
               ),
+
+              const SizedBox(
+                height: 5,
               ),
-              
-              const SizedBox(height: 5,),
 
               // properties
 
               // bungalow
 
-                Center(
+              Center(
                 child: Stack(
                   alignment: Alignment.bottomCenter,
                   children: [
@@ -73,10 +75,7 @@ class HouseListing extends StatelessWidget {
                             Colors.transparent,
                           ],
                         ).createShader(
-                          Rect.fromLTWH(
-                            0, 0, rect.width, rect.height
-                          )
-                        );
+                            Rect.fromLTWH(0, 0, rect.width, rect.height));
                       },
                       blendMode: BlendMode.dstIn,
                       child: ClipRRect(
@@ -86,14 +85,12 @@ class HouseListing extends StatelessWidget {
                           height: 250,
                           width: MediaQuery.of(context).size.width,
                           fit: BoxFit.cover,
-                          
                         ),
                       ),
                     ),
-
-                     Padding(
+                    Padding(
                       padding: const EdgeInsets.symmetric(
-                        horizontal: 10, 
+                        horizontal: 10,
                         vertical: 15,
                       ),
                       child: Row(
@@ -102,47 +99,47 @@ class HouseListing extends StatelessWidget {
                         children: [
                           const Column(
                             children: [
-                              Text("Breeze Estate",
+                              Text(
+                                "Breeze Estate",
                                 style: TextStyle(
                                   fontSize: 16,
                                   color: Color.fromARGB(255, 237, 240, 241),
                                   fontWeight: FontWeight.w600,
-                                  
-                                  ),
                                 ),
-                                Text("Bungalow",
+                              ),
+                              Text(
+                                "Bungalow",
                                 style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                  color: Colors.white
-                                ),
-                                )
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25,
+                                    color: Colors.white),
+                              )
                             ],
                           ),
-                      
-
                           GestureDetector(
-                            onTap: () => Navigator.push(context, 
-                            MaterialPageRoute(
-                              builder: (context) => const Bungalow()
-                              )),
-                              child: const Icon(Icons.arrow_circle_right,
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Bungalow())),
+                            child: const Icon(
+                              Icons.arrow_circle_right,
                               size: 40,
                               color: Colors.white,
                             ),
                           ),
-
                         ],
                       ),
                     ),
-                  ],  
+                  ],
                 ),
               ),
-           
-              const SizedBox(height: 15,),
+
+              const SizedBox(
+                height: 15,
+              ),
 
               // apartments
-               Center(
+              Center(
                 child: Stack(
                   alignment: Alignment.bottomCenter,
                   children: [
@@ -157,10 +154,7 @@ class HouseListing extends StatelessWidget {
                             Colors.transparent,
                           ],
                         ).createShader(
-                          Rect.fromLTWH(
-                            0, 0, rect.width, rect.height
-                          )
-                        );
+                            Rect.fromLTWH(0, 0, rect.width, rect.height));
                       },
                       blendMode: BlendMode.dstIn,
                       child: ClipRRect(
@@ -170,62 +164,59 @@ class HouseListing extends StatelessWidget {
                           height: 250,
                           width: MediaQuery.of(context).size.width,
                           fit: BoxFit.cover,
-                          
                         ),
                       ),
                     ),
-
-                     Padding(
+                    Padding(
                       padding: const EdgeInsets.symmetric(
-                        horizontal: 10, 
-                        vertical: 15
-                      ),
+                          horizontal: 10, vertical: 15),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           const Column(
                             children: [
-                              Text("Kingdom",
+                              Text(
+                                "Kingdom",
                                 style: TextStyle(
                                   fontSize: 16,
                                   color: Color.fromARGB(255, 237, 240, 241),
                                   fontWeight: FontWeight.w600,
-                                  
-                                  ),
                                 ),
-                                Text("Apartments",
+                              ),
+                              Text(
+                                "Apartments",
                                 style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                  color: Colors.white
-                                ),
-                                )
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25,
+                                    color: Colors.white),
+                              )
                             ],
                           ),
 
                           // icon
 
                           GestureDetector(
-                            onTap: () => Navigator.push(context, 
-                            MaterialPageRoute(
-                              builder: (context) => const Apartment()
-                              )),
-                              child: const Icon(Icons.arrow_circle_right,
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Apartment())),
+                            child: const Icon(
+                              Icons.arrow_circle_right,
                               size: 40,
                               color: Colors.white,
                             ),
                           ),
-
-                            
                         ],
                       ),
                     ),
-                  ],  
+                  ],
                 ),
               ),
-             
-              const SizedBox(height: 15,),
+
+              const SizedBox(
+                height: 15,
+              ),
 
               // mansion
               Center(
@@ -243,60 +234,52 @@ class HouseListing extends StatelessWidget {
                             Colors.transparent,
                           ],
                         ).createShader(
-                          Rect.fromLTWH(
-                            0, 0, rect.width, rect.height
-                          )
-                        );
+                            Rect.fromLTWH(0, 0, rect.width, rect.height));
                       },
                       blendMode: BlendMode.dstIn,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(15.0),
                         child: Image.asset(
-                          
                           "lib/images/mansion.png",
                           height: 250,
                           width: MediaQuery.of(context).size.width,
                           fit: BoxFit.cover,
-                          
                         ),
                       ),
                     ),
-
-                     Padding(
+                    Padding(
                       padding: const EdgeInsets.symmetric(
-                        horizontal: 10, 
-                        vertical: 15
-                      ),
+                          horizontal: 10, vertical: 15),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           const Column(
                             children: [
-                              Text("Thy Kings",
+                              Text(
+                                "Thy Kings",
                                 style: TextStyle(
                                   fontSize: 16,
                                   color: Color.fromARGB(255, 237, 240, 241),
                                   fontWeight: FontWeight.w600,
-                                  
-                                  ),
                                 ),
-                                Text("Mansion",
+                              ),
+                              Text(
+                                "Mansion",
                                 style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                  color: Colors.white
-                                ),
-                                )
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25,
+                                    color: Colors.white),
+                              )
                             ],
                           ),
-                      
                           GestureDetector(
-                            onTap: () => Navigator.push(context, 
-                            MaterialPageRoute(
-                              builder: (context) => const Mansion()
-                              )),
-                              child: const Icon(Icons.arrow_circle_right,
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Mansion())),
+                            child: const Icon(
+                              Icons.arrow_circle_right,
                               size: 40,
                               color: Colors.white,
                             ),
@@ -304,16 +287,17 @@ class HouseListing extends StatelessWidget {
                         ],
                       ),
                     ),
-                  ],  
+                  ],
                 ),
               ),
-             
-             const SizedBox(height: 10,),
+
+              const SizedBox(
+                height: 10,
+              ),
             ],
           ),
         ),
       ),
-
     );
   }
 }
